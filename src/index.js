3ea8ae1be842fe61927c4b5235c563341ad211ed
sofyandwis <108184710+sofyandwis@users.noreply.github.com>
2022-07-08 23:31:25 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./app"
-// const nama = ['agus','sofyan','dwi','saputro']
-// function getNama(nama){
-//     return ""+nama;
-// }
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(getNama(nama));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App></App>);
